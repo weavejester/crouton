fa58fc543426413eaf926b2f23502f29464b38a0
James Reeves <jreeves@weavejester.com>
2012-06-05 15:32:15 +0100

    Parse function that returns top element

--- src/crouton/core.clj
+++ src/crouton/core.clj
@@ -1,6 +1,16 @@
-(ns crouton.core)
+(ns crouton.core
+  (:import [org.jsoup Jsoup]
+           [org.jsoup.nodes Document]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defprotocol AsClojure
+  (^:private as-clojure [x] "Turn a Java class into its Clojure equivalent"))
+
+(extend-protocol AsClojure
+  Document
+  (as-clojure [doc]
+    (-> doc .children first)))
+
+(defn parse [source]
+  (-> source
+      Jsoup/parse
+      as-clojure))
