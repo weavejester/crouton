64588927b9a7d997c4fcf6f87cf5ef2989f549ad
James Reeves <jreeves@weavejester.com>
2012-06-05 15:39:51 +0100

    Parse elements into Clojure data

--- src/crouton/core.clj
+++ src/crouton/core.clj
@@ -1,6 +1,6 @@
 (ns crouton.core
   (:import [org.jsoup Jsoup]
-           [org.jsoup.nodes Document]))
+           [org.jsoup.nodes Document Element]))
 
 (defprotocol AsClojure
   (^:private as-clojure [x] "Turn a Java class into its Clojure equivalent"))
@@ -8,7 +8,12 @@
 (extend-protocol AsClojure
   Document
   (as-clojure [doc]
-    (-> doc .children first)))
+    (-> doc .children first as-clojure))
+  Element
+  (as-clojure [element]
+    {:tag (-> element .tagName keyword)
+     :attrs {}
+     :content (->> element .children (map as-clojure))}))
 
 (defn parse [source]
   (-> source
